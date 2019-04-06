0b01efed0f4f33e76f9c89a76ff3105864787e94
crito <crito@blizzlike.org>
2019-04-06 18:54:28 +0200

    fix panic on missing db config for realms

--- modules/endpoints/realm/character/characterlist.go
+++ modules/endpoints/realm/character/characterlist.go
@@ -8,6 +8,7 @@ import (
   "strconv"
   "github.com/gorilla/mux"
 
+  "metagit.org/blizzlike/cmangos-api/modules/database"
   e_account "metagit.org/blizzlike/cmangos-api/modules/endpoints/account"
   cmangos_realm "metagit.org/blizzlike/cmangos-api/cmangos/realmd/realm"
   cmangos_character "metagit.org/blizzlike/cmangos-api/cmangos/mangosd/character"
@@ -24,6 +25,19 @@ func DoCharacterlistByAccount(w http.ResponseWriter, r *http.Request) {
   vars := mux.Vars(r)
   realmid, _ := strconv.Atoi(vars["realm"])
   accountid, _ := strconv.Atoi(vars["account"])
+
+  exists := false
+  for _, v := range database.Mangosd {
+    if realmid == v.Id {
+      exists = true
+    }
+  }
+
+  if !exists {
+    w.WriteHeader(http.StatusBadRequest)
+    return
+  }
+
   for _, v := range realmlist {
     if realmid == v.Id && accountid == id {
       characterlist, err = v.CharacterInstance.GetCharacterByAccountId(accountid)
